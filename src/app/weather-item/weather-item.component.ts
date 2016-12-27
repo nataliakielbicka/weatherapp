ba0d9e17d167cf58f3a0aa90b1e1d7b05061daa6
Natalia <natalia.kielbicka@o2.pl>
2016-12-27 18:11:07 +0100

    second method of input

--- src/app/weather-item/weather-item.component.ts
+++ src/app/weather-item/weather-item.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { WeatherItem } from './weather-item';
 
 @Component({
   selector: 'app-weather-item',
   templateUrl: './weather-item.component.html',
-  styleUrls: ['./weather-item.component.css'],
-  inputs: ['weatherItem: item']
+  styleUrls: ['./weather-item.component.css']
+  //inputs: ['weatherItem: item']
 })
 export class WeatherItemComponent implements OnInit {
-  weatherItem: WeatherItem;
+  @Input('item') weatherItem: WeatherItem;
   constructor() {
     this.weatherItem = new WeatherItem('London','Rainy', 32);
    }
