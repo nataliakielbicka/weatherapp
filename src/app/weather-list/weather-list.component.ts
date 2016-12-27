1c14a85f6cb6faf9225f8582e41aa8c5deb8dda2
Natalia <natalia.kielbicka@o2.pl>
2016-12-27 18:38:27 +0100

    create WeatherService

--- src/app/weather-list/weather-list.component.ts
+++ src/app/weather-list/weather-list.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherItemComponent } from '../weather-item/weather-item.component';
 import { WeatherItem } from '../weather-item/weather-item';
-import { WEATHER_ITEMS } from '../weather-item/weather.data';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-weather-list',
   templateUrl: './weather-list.component.html',
-  styleUrls: ['./weather-list.component.css']
+  styleUrls: ['./weather-list.component.css'],
+  providers: [WeatherService]
 })
 export class WeatherListComponent implements OnInit {
   weatherItems: WeatherItem[];
-  constructor() { }
+  constructor(private _weatherService: WeatherService) { }
 
   ngOnInit():any {
-    this.weatherItems = WEATHER_ITEMS;
+    this.weatherItems = this._weatherService.getWeatherItems();
   }
 
 }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
+import { WEATHER_ITEMS } from './weather-item/weather.data';
 
 @Injectable()
 export class WeatherService {
 
+  getWeatherItems() {
+    return WEATHER_ITEMS;
+  }
   constructor() { }
 
 }
