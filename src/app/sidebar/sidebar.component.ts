813c7d08a2eaeb50147ad301bb8c2c05fce424d6
Natalia <natalia.kielbicka@o2.pl>
2016-12-28 11:15:48 +0100

    create profile service not working yet

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Profile } from '../profile';
+import { ProfileService } from '../profile.service';
+import { WeatherService } from '../weather.service';
+import { WeatherItem } from '../weather-item/weather-item';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
+  providers: [ProfileService, WeatherService]
 })
 export class SidebarComponent implements OnInit {
+  profiles: Profile[];
 
-  constructor() { }
+  constructor(private _profileService: ProfileService, private _weatherService: WeatherService) { }
 
+  onSaveNew() {
+    const cities = this._weatherService.getWeatherItems().map(function (element: WeatherItem) {
+      return element.cityName;
+    })
+    this._profileService.saveNewProfile(cities);
+  }
   ngOnInit() {
+    this.profiles = this._profileService.getProfiles();
   }
 
 }
